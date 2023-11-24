64c5cdfdea02a60fee2f25934237660a575266e2
zhx <630984101@qq.com>
2023-11-24 11:18:02 +0800

    master 3

--- src/main/java/com/example/parent/entity/GitTest.java
+++ src/main/java/com/example/parent/entity/GitTest.java
@@ -13,6 +13,7 @@ public class GitTest {
         System.out.println("on master");
         System.out.println("on hot-fix2");
         System.out.println("on master2222");
+        System.out.println("on master3");
 
     }
 }
